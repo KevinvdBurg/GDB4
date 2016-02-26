2460041df15eee9f11c71033c97319147440408e
Kevin <vanderburg.kevin@gmail.com>
2016-02-26 14:52:35 +0100

    Pizza things

---
+++ GDB4 - the pizza game/Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+    public List<Texture> pic;
+    public GameObject go; //your plane
+                          // Use this for initialization
+    void Start () {
+        ApplyTexture(pic[Random.Range(0, pic.Count)]);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void ApplyTexture(Texture texture)
+    {
+        GetComponent<Renderer>().material.mainTexture = texture;
+    }
+}
